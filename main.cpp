87dd38e7a232671f24c2209ba195e2b8b613f6e2
D. Wegener <dpwegener@gmail.com>
2018-03-10 21:17:26 -0600

    Template to return lambda callback

--- main.cpp
+++ main.cpp
@@ -1,9 +1,18 @@
+#include <functional>
 #include <iostream>
+#include <unordered_map>
 
 struct Event {
     virtual ~Event() {}
     virtual int getId() const = 0;
 };
+
+using EventCall = std::function<void(Event const &)> ;
+
+template<typename T, typename S>
+void acceptEvent(S & service, Event const & event) {
+    service.accept(static_cast<T const &>(event));
+}
 
 struct Foo : Event {
     int getId() const override { return 1; }
@@ -15,6 +24,11 @@ struct Bar : Event {
     std::string address() const { return "43"; };
 };
 
+template<typename E, typename S>
+EventCall eventCallback(S & service) {
+    return [service] (Event const & event) mutable {acceptEvent<E>(service, event);};
+}
+
 struct Service {
     void accept(Foo const & aFoo) {
         std::cout << "You gave me a Foo called " << aFoo.name() << '\n';
@@ -24,10 +38,6 @@ struct Service {
         std::cout << "You gave me a Bar from " << aBar.address() << '\n';
     }
 };
-template<typename T, typename S>
-void acceptEvent(S & service, Event const & event) {
-    service.accept(static_cast<T const &>(event));
-}
 
 struct Dispatcher {
     Dispatcher(Service & aService) : myService(aService) {};
@@ -57,6 +67,12 @@ int main(int, char**) {
     Service theService;
     theService.accept(foo);
     theService.accept(bar);
+    std::unordered_map<int, std::function<void(Event const &)>> callbacks;
+    callbacks[1] = eventCallback<Foo>(theService);
+    callbacks[2] = eventCallback<Bar>(theService);
+    callbacks[1](foo);
+    callbacks[2](bar);
+    eventCallback<Foo>(theService)(foo);
     Dispatcher dispatcher(theService);
     dispatcher.accept(bar);
     dispatcher.accept(foo);
